f161dea9d866d3e0a15ded396837424c9c8b47ab
An Nguyen <navdevengr@gmail.com>
2023-09-29 19:31:11 -0500

    unit 5 project on redux

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import potentialCountriesReducer from "../redux/slices/potentialCountriesSlice";
+
+import displayedCountryReducer from "../redux/slices/displayCountrySlice";
+
+
+export default configureStore({
+    reducer: {
+        potentialCountries: potentialCountriesReducer,
+        displayedCountry: displayedCountryReducer,
+    },
+});
